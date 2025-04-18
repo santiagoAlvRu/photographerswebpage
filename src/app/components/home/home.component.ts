34dbb00679e391bbae8a6d64e943fe0c0517e10b
Shohre <shohre.shadman@yahoo.com>
2025-04-18 14:44:11 +0200

    add hero components action

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Iawards, Iphotos } from '../../iphotos';
 import { awards, photos } from '../../photos';
 import { RouterLink } from '@angular/router';
+import {HeroComponent} from '../../hero/hero.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
