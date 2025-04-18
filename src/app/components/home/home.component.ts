82bc247a46a09df5be6e5e2e674786841a190f56
yobraj <lamayobraj@gmail.com>
2025-04-18 22:57:10 +0200

    contact component added in the master.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,10 +4,11 @@ import { awards, photos } from '../../photos';
 import { RouterLink } from '@angular/router';
 import {HeroComponent} from '../hero/hero.component';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
+import { ContactUsComponent } from "../../contact-us/contact-us.component";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, HeroComponent, TestimonialsComponent],
+  imports: [RouterLink, HeroComponent, TestimonialsComponent, ContactUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
