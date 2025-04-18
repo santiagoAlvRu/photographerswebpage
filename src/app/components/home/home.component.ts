3f0b0da29bb825cdb4d8416734237413de02e2c8
Shohre <shohre.shadman@yahoo.com>
2025-04-18 20:10:48 +0200

    Move the hero component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Iawards, Iphotos } from '../../iphotos';
 import { awards, photos } from '../../photos';
 import { RouterLink } from '@angular/router';
-import {HeroComponent} from '../../hero/hero.component';
+import {HeroComponent} from '../hero/hero.component';
 
 @Component({
   selector: 'app-home',
