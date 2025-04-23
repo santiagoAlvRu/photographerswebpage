b226e602f7a148c4bbf375f607937cc4c2ad688d
santiagoAlvRu <santiagoesluz@gmail.com>
2025-04-23 19:25:46 +0200

    when user click read more(home) the page about is displayed in the top of the page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,9 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./about.component.css'],
   imports: [RouterLink],
 })
-export class AboutComponent {
-  
+
+export class AboutComponent implements OnInit {
+  ngOnInit(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
