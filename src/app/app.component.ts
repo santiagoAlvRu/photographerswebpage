d6530a16f4a3e176089c872d6ba9c4d0228c6938
santiagoAlvRu <santiagoesluz@gmail.com>
2025-04-24 19:33:44 +0200

    final details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LayoutComponent} from './components/layout/layout.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent, NgbModule], //Import NgbModule here to use it in the app
+  imports: [RouterOutlet, LayoutComponent, CommonModule, ReactiveFormsModule, NgbModule], //Import NgbModule here to use it in the app
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
